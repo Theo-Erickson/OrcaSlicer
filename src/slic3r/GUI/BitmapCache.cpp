85e151a1885576438fc8691c36bf6f2dbd0a052a
Thelonius Erickson <theoserickson@gmail.com>
2026-03-15 20:43:23 -0700

    Test Change Main Orca Color
    This was done manually, and has the former color commented next to it, as a test. Further developments aim to make this a user customizable value and expose a color picker in the UI.

--- src/slic3r/GUI/BitmapCache.cpp
+++ src/slic3r/GUI/BitmapCache.cpp
@@ -335,7 +335,7 @@ wxBitmap* BitmapCache::load_svg(const std::string &bitmap_name, unsigned target_
         replaces["\"#6B6B6B\""] = "\"#818182\"";
         replaces["\"#909090\""] = "\"#FFFFFF\"";
         replaces["\"#00FF00\""] = "\"#FF0000\"";
-        replaces["\"#009688\""] = "\"#00675b\"";
+        replaces["\"#009688\""] = "\"#00ffe4\"";//
         replaces["\"#F1F1F1\""] = "\"#36363B\"";
         replaces["#DBDBDB"] = "#4A4A51"; // ORCA border color
         replaces["#F0F0F1"] = "#333337"; // ORCA disabled background color
@@ -345,7 +345,7 @@ wxBitmap* BitmapCache::load_svg(const std::string &bitmap_name, unsigned target_
     }
 
     if (strstr(bitmap_name.c_str(), "toggle_on") != NULL && dark_mode) // ORCA only replace color of toggle button
-        replaces["#009688"] = "#00675b";
+        replaces["#009688"] = "#f2a450" /*00675b*/;
 
     if (!new_color.empty())
         replaces["\"#009688\""] = "\"" + new_color + "\"";
