9eb38b509877f9001558371dcb4816c277284bf3
Thelonius Erickson <theoserickson@gmail.com>
2026-03-15 20:41:17 -0700

    Change Home Button Icon In Top Tab Buttons

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1286,7 +1286,7 @@ void MainFrame::init_tabpanel() {
             select_tab(MainFrame::tpHome);
             m_webview->load_url(url);
         });
-        m_tabpanel->AddPage(m_webview, "", "tab_home_active", "tab_home_active", false);
+        m_tabpanel->AddPage(m_webview, "", "tab_home_active", "tab_home_inactive", false);
         m_param_panel = new ParamsPanel(m_tabpanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBK_LEFT | wxTAB_TRAVERSAL);
     }
 
