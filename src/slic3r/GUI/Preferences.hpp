5870e08818b5a4bbd53aa36d7ba83a1a5d94e66a
Thelonius Erickson <theoserickson@gmail.com>
2026-03-16 02:42:55 -0700

    Add color picker panel code & clean up single color picker function
    create_item_color_picker_panel(): creates a set of buttons, from a passed in vector of a simple struct with label, light color, and dark color.
    create_item_color_picker_single(): rework of create_item_color_picker() without the custom return struct and with cleaned up internals and signatures

--- src/slic3r/GUI/Preferences.hpp
+++ src/slic3r/GUI/Preferences.hpp
@@ -31,24 +31,13 @@ namespace Slic3r { namespace GUI {
 class CheckBox;
 class TextInput;
 
- struct ColourPickerInfo
+struct ThemeColor
 {
-    wxBoxSizer*                        sizer;
-    std::shared_ptr<wxColourData>      colourData;  // Automatic lifetime
-    wxPanel*                           colorIcon;   // colored square button
-    Button*                            resetButton; // appears on color change
-    std::function<void(wxColourData&)> onColorSet;
+    wxString label;
+    wxColour lightColor;
+    wxColour darkColor;
 
-    // Default constructor - safe initialization
-    ColourPickerInfo() : sizer(nullptr), colourData(nullptr), colorIcon(nullptr), resetButton(nullptr) {}
-
-    // Parametrized constructor
-    ColourPickerInfo(wxBoxSizer* s, wxColourData* d, wxPanel* icon, Button* reset, std::function<void(wxColourData&)> cb)
-        : sizer(s), colourData(d), colorIcon(icon), resetButton(reset), onColorSet(std::move(cb))
-    {}
-
-    // Validity Function
-    bool IsValid() { return sizer || colourData || colorIcon || resetButton || onColorSet; }
+    ThemeColor(const wxString& l, const wxColour& light, const wxColour& dark) : label(l), lightColor(light), darkColor(dark) {}
 };
 
 class PreferencesDialog : public DPIDialog
@@ -113,7 +102,8 @@ public:
     wxBoxSizer *create_item_darkmode(wxString title,wxString tooltip, std::string param);
     void set_dark_mode();
     wxBoxSizer *create_item_button(wxString title, wxString title2, wxString tooltip, wxString tooltip2, std::function<void()> onclick);
-    ColourPickerInfo create_item_clrPicker(wxString title, wxString title2, wxString tooltip, wxString tooltip2, std::function<void(wxColourData&)> onColorSet, wxColour defaultColor = *wxWHITE);  // optional default);
+    wxBoxSizer *create_item_color_picker_single(wxString labelTitle, wxString resetButtonTitle,wxString labelTooltip, wxString resetButtonTooltip, std::function<void(wxColourData&)> onColorSet, wxColour defaultColor = *wxWHITE);  // optional default);
+    wxBoxSizer *create_item_color_picker_panel(const std::vector<ThemeColor>& colorsToPickFrom);
     wxBoxSizer *create_item_downloads(wxString title, wxString tooltip);
     wxBoxSizer *create_item_input(wxString title, wxString title2, wxString tooltip, std::string param, std::function<void(wxString)> onchange = {});
     wxBoxSizer *create_item_spinctrl(wxString title, wxString title2, wxString side_label, wxString tooltip, std::string param, int min, int max, std::function<void(int)> onchange = nullptr);
