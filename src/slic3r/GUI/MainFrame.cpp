7d77a2c03b8a95e9697d4cccc3083de158e2c3c9
Thelonius Erickson <theoserickson@gmail.com>
2026-03-26 00:51:44 -0700

    Resolve Main Frame merge conflict around new history panel icon

--- src/slic3r/GUI/MainFrame.cpp
+++ src/slic3r/GUI/MainFrame.cpp
@@ -1344,7 +1344,8 @@ void MainFrame::init_tabpanel() {
 
     m_history_panel = new HistoryPanel(m_tabpanel);
     m_history_panel->SetBackgroundColour(*wxWHITE);
-    m_tabpanel->AddPage(m_history_panel, _L("History"), std::string("tab_auxiliary_active"), std::string("tab_auxiliary_active"), false);
+
+    m_tabpanel->AddPage(m_history_panel, _L("History"), std::string("tab_history_pages_static"), std::string("tab_history_pages_static"), false);
 }
 
 // SoftFever
